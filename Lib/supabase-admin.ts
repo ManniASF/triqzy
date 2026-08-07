a60c76c7b4430e4655fba3c81cc267f34b15132f
ManniASF <paulakachukwu238@gmail.com>
2026-08-07 04:34:21 +0100

    fix supabase configuration and admin build

--- Lib/supabase-admin.ts
+++ Lib/supabase-admin.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const url = process.env.https//bxokeiaccwcceqoakixu.supabase.co;
 const key = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!url || !key) {
